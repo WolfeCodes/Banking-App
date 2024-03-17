185db8f9c9f2b60e492285ac97f233ec39c7c676
unknown <marc.wolfe21@gmail.com>
2024-03-17 09:08:49 -0500

    Add withdraw REST API functionality

--- src/main/java/com/wolfecodes/bankingapp/service/impl/AccountServiceimpl.java
+++ src/main/java/com/wolfecodes/bankingapp/service/impl/AccountServiceimpl.java
@@ -42,4 +42,19 @@ public class AccountServiceimpl implements AccountService {
         Account savedAccount = accountRepository.save(account);
         return AccountMapper.mapToAccountDto(savedAccount);
     }
+
+    @Override
+    public AccountDto withdraw(Long id, Double amount) {
+        Account account = accountRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Account does not exist"));
+        if (account.getBalance() < amount) {
+            throw new RuntimeException("Insufficient balance");
+        }
+        double total = account.getBalance() - amount;
+        account.setBalance(total);
+        Account savedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
 }
